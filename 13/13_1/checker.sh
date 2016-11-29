205da7993ef5f769451fe3dc67442521b3d7ccbf
HeeHoon Kim <csehydrogen@gmail.com>
2016-11-29 19:00:42 +0900

    checker modified

--- 13/13_1/checker.sh
+++ 13/13_1/checker.sh
@@ -1,3 +1,19 @@
 # h0 r5 r10 r50 r100 r500 r1000 r5000 r10000
+./a.out h0.in answer.txt
+./compare h0.out answer.txt
 ./a.out r5.in answer.txt
 ./compare r5.out answer.txt
+./a.out r10.in answer.txt
+./compare r10.out answer.txt
+./a.out r50.in answer.txt
+./compare r50.out answer.txt
+./a.out r100.in answer.txt
+./compare r100.out answer.txt
+./a.out r500.in answer.txt
+./compare r500.out answer.txt
+./a.out r1000.in answer.txt
+./compare r1000.out answer.txt
+./a.out r5000.in answer.txt
+./compare r5000.out answer.txt
+./a.out r10000.in answer.txt
+./compare r10000.out answer.txt
