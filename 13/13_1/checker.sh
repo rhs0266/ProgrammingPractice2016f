9a3dc9ae8e13b6e8a2eed80c04a856efa494b317
HeeHoon Kim <csehydrogen@gmail.com>
2016-11-29 18:53:37 +0900

    TC for 13

---
+++ 13/13_1/checker.sh
@@ -0,0 +1,3 @@
+# h0 r5 r10 r50 r100 r500 r1000 r5000 r10000
+./a.out r5.in answer.txt
+./compare r5.out answer.txt
---
+++ 13/13_2/checker.sh
@@ -0,0 +1,3 @@
+# h0 h1 r0 r1 r2 r3 r4
+./a.out h0.in h0.pat answer.txt
+./compare h0.out answer.txt
